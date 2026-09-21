47c9c3a97f459162029c64c9db909e4613d1405b
Praveen Soni <praveenkumar12032@gmail.com>
2026-09-22 00:03:53 +0530

    Time: 0 ms (100%), Space: 19.4 MB (19.83%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,12 +1,29 @@
 class Solution:
     def plusOne(self, digits: list[int]) -> list[int]:
-        stri = ""
-        for i in digits:
-            stri += str(i)
+        # basic version 
+
+        # stri = ""
+        # for i in digits:
+        #     stri += str(i)
         
-        temp = str(int(stri)+1)
-        res = []
-        for i in temp:
-            res.append(int(i))
-        return res
+        # temp = str(int(stri)+1)
+        # res = []
+        # for i in temp:
+        #     res.append(int(i))
+        # return res
+
+        # improved version
+        
+        nums = 0
+        for digit in digits:
+            nums = nums*10+digit
+
+        nums += 1
+        ans = []
+        while nums>0:
+            temp = nums % 10
+            ans.append(temp)
+            nums //= 10
+
+        return ans[::-1]
         
